6c28cc8f7cc41d92017124c9e722cc706f802f4b
Cat1101 <kotk4608@mail.ru>
2026-04-26 16:22:01 +0300

    Added translator based on google api

---
+++ SimpleTGBot/Translator.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace SimpleTGBot;
+
+public class Translator
+{
+    /// <summary>
+    /// Переводить текст через Google Translate
+    /// </summary>
+    /// <param name="msg">Сообщение</param>
+    /// <param name="from">Язык оригинала</param>
+    /// <param name="to">Язык перевода</param>
+    public static async Task<string> Translate(string msg, string from="ru", string to="en")
+    {
+        using var client = new HttpClient();
+        var result = "";
+        
+        foreach (var ss in msg.Split(new []{'\n', '.', '\r'},
+                     StringSplitOptions.RemoveEmptyEntries))
+        {
+            var url = $"https://translate.googleapis.com/translate_a/single" +
+                      $"?client=gtx&sl={from}&tl={to}&dt=t&q={Uri.EscapeDataString(ss)}";
+            var response = await client.GetStringAsync(url);
+            var json = JsonDocument.Parse(response);
+            result += "\n" + json.RootElement[0][0][0].GetString();
+        }
+
+        return result;
+    }
+}
